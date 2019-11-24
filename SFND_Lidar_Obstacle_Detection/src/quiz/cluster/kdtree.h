f47160b8b02558e3fa5af867d0a27e336752566a
Cedric-Perauer <Ceddyperauer@gmail.com>
2019-11-24 03:30:56 +0100

    KD Tree Search added

--- SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
+++ SFND_Lidar_Obstacle_Detection/src/quiz/cluster/kdtree.h
@@ -36,7 +36,7 @@ struct KdTree
 		else
 		{
           bool even = (depth%2); //using bool, if value is even even=0 and then x is used, y if otherwise
-
+          
 		  point[even] >= (*node)->point[even] ? helper_insert(&((*node)->right),point,id,depth+1) : helper_insert(&((*node)->left),point,id,depth+1);
 		     
 		}
@@ -53,10 +53,41 @@ struct KdTree
         
 	}
 
+	float distance(std::pair<float,float> a, std::pair<float,float> b)
+	{
+		return sqrt(pow(a.first-b.first,2)+pow(a.second-b.second,2));
+	}
+
+	void search_helper(std::vector<float> target, Node *node, std::vector<int> &ids, int depth, float Tol) 
+	{
+		if(node!=NULL)
+		{   //see if point is within Threshold
+			if((node->point[0]>= (target[0]-Tol)) && (node->point[0] <= (target[0]+Tol)) && (node->point[1]>= (target[1]-Tol)) && (node->point[1] <= (target[1]+Tol)))  
+			{  
+                 float d = distance(std::pair<float,float> {node->point[0],node->point[1]}, std::pair<float,float> {target[0],target[1]});
+				 if(d <= Tol)
+				 {
+					 ids.emplace_back(node->id);
+				 }
+			}
+
+			if((target[depth%2]-Tol) < node->point[depth%2])
+			{
+				search_helper(target,node->left,ids,depth+1,Tol);
+			}
+
+			if((target[depth%2]+Tol) > node->point[depth%2])
+			{
+				search_helper(target,node->right,ids,depth+1,Tol);
+			}
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
+	{   
 		std::vector<int> ids;
+		search_helper(target, root, ids, 0, distanceTol);
 		return ids;
 	}
 	
